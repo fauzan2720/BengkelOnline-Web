30c92c099aecdf120e2ed043724d2e8c0b6987a3
Afris Nurfal aziz <74905155+afrisnurfalaziz@users.noreply.github.com>
2022-06-07 15:26:01 +0700

    update login

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -53,7 +53,7 @@
                                 <div class="card-header pb-0 text-start">
                                     <h4 class="font-weight-bolder">Login</h4>
                                     @if (session()->has('error'))
-                                        <p class="text-danger">{{ session('error') }}</p>
+                                    <p class="text-danger">{{ session('error') }}</p>
                                     @endif
                                     {{-- @if (session()->has('success'))
                                         <p class="text-success">{{ session('success') }}</p>
@@ -80,8 +80,8 @@
                             <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('https://user-images.githubusercontent.com/74905155/165243311-93d1d93e-0536-4369-93a0-53c9e133c842.jpg');
                                 background-size: cover;">
                                 <span class="mask bg-gradient-primary opacity-6"></span>
-                                <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new currency"</h4>
-                                <p class="text-white position-relative">The more effortless the writing looks, the more effort the writer actually put into the process.</p>
+                                <h4 class="mt-5 text-white font-weight-bolder position-relative">"Welcome Bengkel Online"</h4>
+                                <p class="text-white position-relative">Silahkan Login untuk memulai.</p>
                             </div>
                         </div>
                     </div>
